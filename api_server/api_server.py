77db9f131fec658002ae5f35490c9a6764d5df8f
christianhilscher <christian.hilscher@protonmail.com>
2024-01-02 22:04:00 +0100

    feat: new port

--- api_server/api_server.py
+++ api_server/api_server.py
@@ -11,4 +11,4 @@ def send_data():
     return jsonify({"message": "Data sent to processor"})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=80)
