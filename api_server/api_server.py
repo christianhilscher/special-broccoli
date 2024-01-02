c303f8ade06e0c9eafccacbed8c02d6b76fd387c
Christian Hilscher <42186988+christianhilscher@users.noreply.github.com>
2024-01-02 21:05:03 +0100

    Initial commit

---
+++ api_server/api_server.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, jsonify
+import requests
+
+app = Flask(__name__)
+
+@app.route('/send-data', methods=['POST'])
+def send_data():
+    data = request.json
+    # Send data to the data-processor container
+    requests.post('http://data-processor:5001/process', json=data)
+    return jsonify({"message": "Data sent to processor"})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
