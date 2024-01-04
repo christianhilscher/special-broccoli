4640ba42ceb13563f06f14c8eb27144c45923902
christianhilscher <christian.hilscher@protonmail.com>
2024-01-04 22:15:18 +0100

    f

--- retrainer/train_model.py
+++ retrainer/train_model.py
@@ -2,7 +2,9 @@ import os
 
 def train_model(identifier):
     print(f"Model trained successfully with identifier: {identifier}")
-    model_filename = f"model-{identifier}.pkl"
+    model_filename = f"/home/ec2-user/models/model-{identifier}.pkl"
+    with open(model_filename, "w") as file:
+        file.write("dummy model data")
 
 if __name__ == "__main__":
     identifier = os.getenv("IDENTIFIER", "default-identifier")
