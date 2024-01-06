4b6d96cffc7d8b7a61153f7cef209c729814523c
christianhilscher <christian.hilscher@protonmail.com>
2024-01-06 23:22:29 +0100

    f

--- api_server/api_server.py
+++ api_server/api_server.py
@@ -1,21 +1,11 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from sklearn.linear_model import LinearRegression
-import numpy as np
 
-# Dummy model training for demonstration
-X, y = np.array([[1], [2], [3], [4], [5]]), np.array([1, 2, 3, 4, 5])
-model = LinearRegression().fit(X, y)
-
-# Define request schema
 class PredictionRequest(BaseModel):
-    data: list  # List of input features
+    data: list
 
 app = FastAPI()
 
 @app.post("/predict")
 async def make_prediction(request: PredictionRequest):
-    # Convert request data to numpy array for prediction
-    input_data = np.array(request.data).reshape(-1, 1)
-    prediction = model.predict(input_data)
-    return {"prediction": prediction.tolist()}
+    return {"prediction": request.data}
--- docker/api_server.Dockerfile
+++ docker/api_server.Dockerfile
@@ -1,17 +1,16 @@
-# Set the working directory in the container to /app
+FROM python:3.11
+
 WORKDIR /app
 
-# Copy the current directory contents into the container at /app
-COPY . /app
+COPY pyproject.toml poetry.lock* /app/
 
-# Install any needed packages specified in requirements.txt
-RUN pip install --no-cache-dir fastapi uvicorn scikit-learn
+RUN pip install --no-cache-dir poetry
 
-# Make port 8000 available to the world outside this container
+RUN poetry config virtualenvs.create false
+RUN poetry install
+
+COPY /api_server/api_server.py /app
+
 EXPOSE 8000
 
-# Define environment variable
-ENV NAME World
-
-# Run app.py when the container launches
 CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
