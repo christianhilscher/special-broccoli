4826134c035483fc7c0119f86695af75420584c6
christianhilscher <christian.hilscher@protonmail.com>
2024-01-10 21:02:31 +0100

    f

--- smart_buildings/retrain/drift_monitor.py
+++ smart_buildings/retrain/drift_monitor.py
@@ -1,5 +1,7 @@
 import json
 from typing import List, Dict
+import subprocess
+import os
 
 import polars as pl
 import requests
@@ -40,7 +42,28 @@ def compare_metrics_against_thresholds(
 
 
 def trigger_retraining():
-    print("Needs retraining")
+    repo_owner = "christianhilscher"
+    repo_name = os.getenv("REPOSITORY_NAME", "")
+    issue_title = "Drift-Monitor"
+    issue_body = "Test Body for Drift-Monitor"
+    access_token = os.getenv("GIT_TOKEN", "")
+
+    # Construct the curl command
+    curl_command = (
+        f'curl -L -X POST -H "Accept: application/vnd.github.v3+json" '
+        f'-H "Authorization: Bearer {access_token}" '
+        f'-H "X-GitHub-Api-Version: 2022-11-28" '
+        f"https://api.github.com/repos/{repo_owner}/{repo_name}/issues "
+        f"-d \"{{'title':'{issue_title}','body':'{issue_body}'}}\""
+    )
+
+    # Execute the curl command using subprocess
+    try:
+        subprocess.run(curl_command, shell=True, check=True)
+        print("GitHub issue created successfully.")
+    except subprocess.CalledProcessError as e:
+        print(f"Failed to create GitHub issue. Return code: {e.returncode}")
+        print(e.output.decode())
 
 
 if __name__ == "__main__":
@@ -54,6 +77,7 @@ if __name__ == "__main__":
     model_drift = compare_metrics_against_thresholds(
         metrics=model_evaluation_metrics, thresholds=config["thresholds"]
     )
+    model_drift = True
 
     if model_drift:
         trigger_retraining()
