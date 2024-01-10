fb76a93f75f8282f24c09bfec03945dd997a682d
Christian Hilscher <42186988+christianhilscher@users.noreply.github.com>
2024-01-10 23:10:41 +0100

    feat: drift-monitor (#21)

--- smart_buildings/retrain/drift_monitor.py
+++ smart_buildings/retrain/drift_monitor.py
@@ -1,11 +1,71 @@
+import json
+from typing import List, Dict
+import subprocess
+import os
+
+import polars as pl
 import requests
-from train import read_data
+from retrain.train import select_target
+from retrain.utils import read_data, get_config
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
+
+
+def get_predictions(data: pl.DataFrame) -> List[int]:
+    data_dict = {"data": data.to_dict(as_series=False)}
+    api_server_url = os.getenv("API_SERVER_URL", "http://api-server:5000/predict")
+
+    response = json.loads(requests.post(api_server_url, json=data_dict).text)
+    return pl.Series(name="predictions", values=response["prediction"])
+
+
+def get_model_evaluation_metrics(
+    y_test: pl.Series, y_pred: pl.Series
+) -> Dict[str, float]:
+    metric_dict = {
+        "accuracy": accuracy_score(y_test, y_pred),
+        "precision": precision_score(y_test, y_pred),
+        "recall": recall_score(y_test, y_pred),
+        "f1-scrore": f1_score(y_test, y_pred),
+    }
+    print(metric_dict)
+    return metric_dict
+
+
+def get_data_evaluation_metrics():
+    pass
+
+
+def compare_metrics_against_thresholds(
+    metrics: Dict[str, float], thresholds: Dict[str, float]
+) -> bool:
+    return any(
+        metrics.get(metric, 0) < threshold for metric, threshold in thresholds.items()
+    )
+
+
+def trigger_retraining():
+    try:
+        # Run the Bash script
+        subprocess.run(["/bin/bash", "/app/drift_monitor/raise_issue.sh"], check=True)
+        print("GitHub issue created successfully.")
+    except subprocess.CalledProcessError as e:
+        print(f"Failed to create GitHub issue. Return code: {e.returncode}")
+        if e.output:
+            print(e.output.decode())
+
 
 if __name__ == "__main__":
-    data_path = "~/Downloads/data/datatraining.txt"
-    data = read_data(data_path)
-    data_dict = {"data": data.sample(5).to_dict(as_series=False)}
-    url = "http://localhost:5000/predict"
+    config = get_config("/app/config/config.yaml")
+    latest_data = read_data(config["test_data_path"])
 
-    response = requests.post(url, json=data_dict)
-    print(response.text)
+    predictions = get_predictions(latest_data)
+    real_values = select_target(latest_data, config["target"])
+
+    model_evaluation_metrics = get_model_evaluation_metrics(real_values, predictions)
+    model_drift = compare_metrics_against_thresholds(
+        metrics=model_evaluation_metrics, thresholds=config["thresholds"]
+    )
+    model_drift = True
+
+    if model_drift:
+        trigger_retraining()
--- smart_buildings/retrain/train.py
+++ smart_buildings/retrain/train.py
@@ -1,22 +1,18 @@
-import polars as pl
-import lightgbm as lgb
-from sklearn.model_selection import train_test_split
-from typing import List, Tuple, Dict, Optional
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
-import pickle
-import pathlib
 import os
+import pathlib
+import pickle
+from typing import Dict, List, Optional
+
+from retrain.utils import get_config, read_data
 
 
+import lightgbm as lgb
+import polars as pl
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
+from sklearn.model_selection import train_test_split
 
-def read_data(path:str)->pl.DataFrame:
-    data = pl.read_csv(path,has_header=False, skip_rows=1)
-    header = pl.read_csv(path, n_rows=1, has_header=False, truncate_ragged_lines=True)
-    data = data[:, 1:]
-    data.columns = header.row(0)
-    return data
 
-def process_data(data:pl.DataFrame)->pl.DataFrame:
+def process_data(data: pl.DataFrame) -> pl.DataFrame:
     data = data.with_columns(pl.col("date").str.to_datetime())
     data = data.with_columns(
         (pl.col("date").dt.hour().alias("hour_of_day")),
@@ -26,64 +22,66 @@ def process_data(data:pl.DataFrame)->pl.DataFrame:
     )
     return data
 
-def select_features(data:pl.DataFrame, features:List[str])->pl.DataFrame:
+
+def select_features(data: pl.DataFrame, features: List[str]) -> pl.DataFrame:
     X = data.select(features)
     return X
 
-def select_target(data:pl.DataFrame, target:str)->pl.Series:
+
+def select_target(data: pl.DataFrame, target: str) -> pl.Series:
     return data.select(target).to_series()
 
-def train_model(X:pl.DataFrame, y:pl.Series, param_dict:Optional[Dict]=None)->lgb.LGBMClassifier:
-    
+
+def train_model(
+    X: pl.DataFrame, y: pl.Series, param_dict: Optional[Dict] = None
+) -> lgb.LGBMClassifier:
     clf = lgb.LGBMClassifier(
-        boosting_type='gbdt',
-        objective='binary',
+        boosting_type="gbdt",
+        objective="binary",
         num_leaves=31,
         learning_rate=0.05,
         feature_fraction=0.9,
         bagging_fraction=0.8,
-        bagging_freq=5
+        bagging_freq=5,
     )
 
     clf.fit(X, y)
     return clf
 
+
 def make_predictions(X, trained_model):
     return trained_model.predict(X)
 
-def evaluate_predictions(y_test:pl.Series, y_pred:pl.Series)-> None:
-    print("Accuracy:", round(accuracy_score(y_test, y_pred),2))
-    print("Precision:", round(precision_score(y_test, y_pred),2))
-    print("Recall:", round(recall_score(y_test, y_pred),2))
-    print("F1 Score:", round(f1_score(y_test, y_pred),2))
 
-def save_trained_model(
-        path:str, 
-        trained_model:lgb.LGBMClassifier,
-        id:str
-        )->None:
-    
+def evaluate_predictions(y_test: pl.Series, y_pred: pl.Series) -> None:
+    print("Accuracy:", round(accuracy_score(y_test, y_pred), 2))
+    print("Precision:", round(precision_score(y_test, y_pred), 2))
+    print("Recall:", round(recall_score(y_test, y_pred), 2))
+    print("F1 Score:", round(f1_score(y_test, y_pred), 2))
+
+
+def save_trained_model(path: str, trained_model: lgb.LGBMClassifier, id: str) -> None:
     path = pathlib.Path(path)
     path.mkdir(exist_ok=True, parents=True)
     model_path = str(path / f"model-{id}.pkl")
-    with open(model_path, 'wb') as file:
+    with open(model_path, "wb") as file:
         pickle.dump(trained_model, file)
 
-def run(data_path:str, model_path:str, id:str):
+
+def run_training(data_path: str):
     data = read_data(path=data_path)
     data = process_data(data=data)
-    X= select_features(data=data, features=["Light"])
+    X = select_features(data=data, features=["Light"])
     y = select_target(data=data, target="Occupancy")
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5)
     trained_model = train_model(X=X_train, y=y_train)
     predictions = make_predictions(X=X_test, trained_model=trained_model)
     evaluate_predictions(y_test=y_test, y_pred=predictions)
-    save_trained_model(path=model_path, trained_model=trained_model, id=id)
-
 
 
 if __name__ == "__main__":
     id = os.getenv("IDENTIFIER", "default-identifier")
-    data_path = "/data/datatraining.txt"
-    model_path = "/models"
-    run(data_path, model_path, id)
+    config = get_config("/app/config/config.yaml")
+
+    trained_model = run_training(config["train_data_path"])
+    save_trained_model(config["model_path"], trained_model, id)
