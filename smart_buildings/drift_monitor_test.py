f518c432bd984cecbd65a2da2632fa9201834815
christianhilscher <christian.hilscher@protonmail.com>
2024-01-13 14:02:20 +0100

    refactor: first cleaning of repo

--- smart_buildings/drift_monitor_test.py
+++ smart_buildings/drift_monitor_test.py
@@ -5,7 +5,8 @@ if __name__ == "__main__":
     data_path = "~/Downloads/data/datatest.txt"
     data = read_data(data_path)
     data_dict = {"data": data.sample(5).to_dict(as_series=False)}
-    url = "http://localhost:5000/predict"
+    url = "http://51.20.8.35:80/predict"
+    # url = "http://localhost:5000/predict"
 
     response = requests.post(url, json=data_dict)
     print(response.text)
